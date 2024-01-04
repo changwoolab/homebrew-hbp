6921aa439e9ffb64558058c3aca8fb4982c39ceb
changwoolab <cwyoo01@naver.com>
2024-01-04 11:34:30 +0900

    pub

--- Formula/hbp.rb
+++ Formula/hbp.rb
@@ -1,11 +1,12 @@
 class Hbp < Formula
     desc "Changwoolab's CLI utilities for avoiding boilerplates"
     homepage "https://github.com/changwoolab/hate-boilerplates"
-    url "https://github.com/changwoolab/hate-boilerplates/releases/download/v0.2.0/hbp-mac.tar.gz"
-    sha256 "787762cb50a5ea5fce51793af0eeca88ec438d3f13ff1c2752790303c6a1f8c6"
-    version "0.2.1"
+    url "https://github.com/changwoolab/hate-boilerplates/releases/download/v0.3.0/hbp.tar.gz"
+    sha256 "b131bd9847ca29f7f5544db9b68110fa1df89569587d217ac99e756b6af48512"
+    version "0.3.0"
   
     def install
       bin.install "hbp"
     end
   end
+  
